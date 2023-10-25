b529652eb60a0f36dcb2600f1ee6806d6b23c772
AbrahamDiaw <diawabrahampro@gmail.com>
2023-10-25 17:50:03 +0200

    Changes by dalil

--- server/lib/time_manager_web/controllers/user/user_controller.ex
+++ server/lib/time_manager_web/controllers/user/user_controller.ex
@@ -1,18 +1,18 @@
 defmodule TIME_MANAGERWeb.UserController do
   use TIME_MANAGERWeb, :controller
 
-  alias TIME_MANAGER.Models
+  alias TIME_MANAGER.UserRepo
   alias TIME_MANAGER.Models.User
 
   action_fallback TIME_MANAGERWeb.FallbackController
 
   def index(conn, _params) do
-    users = Models.list_users()
+    users = UserRepo.list_users()
     render(conn, :index, users: users)
   end
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Models.create_user(user_params) do
+    with {:ok, %User{} = user} <- UserRepo.create_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", ~p"/api/users/#{user}")
@@ -21,23 +21,23 @@ defmodule TIME_MANAGERWeb.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Models.get_user!(id)
+    user = UserRepo.get_user!(id)
     render(conn, :show, user: user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Models.get_user!(id)
+    user = UserRepo.get_user!(id)
     # TODO : manage error when user not found
 
-    with {:ok, %User{} = user} <- Models.update_user(user, user_params) do
+    with {:ok, %User{} = user} <- UserRepo.update_user(user, user_params) do
       render(conn, :show, user: user)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    user = Models.get_user!(id)
+    user = UserRepo.get_user!(id)
 
-    with {:ok, %User{}} <- Models.delete_user(user) do
+    with {:ok, %User{}} <- UserRepo.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
   end
