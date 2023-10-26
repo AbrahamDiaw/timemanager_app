a3b263c20481677545a554992808fe55e9fca458
AbrahamDiaw <diawabrahampro@gmail.com>
2023-10-26 14:09:29 +0200

    Changes by jean-francois

---
+++ client/src/components/pages/WonkingTimes.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+export default {
+  name: "WorkingTimes",
+  props: {
+
+  },
+  data() {
+      return {
+        data: []
+      }
+  },
+  components: {
+
+  },
+  mounted() {
+    const testData = [
+        {id:1,start:"", end: "", user: 1},
+        {id:2,start:"", end: "", user: 1},
+        {id:3,start:"", end: "", user: 1},
+        {id:4,start:"", end: "", user: 2},
+        {id:5,start:"", end: "", user: 2},
+        {id:6,start:"", end: "", user: 2},
+        {id:7,start:"", end: "", user: 2},
+        {id:8,start:"", end: "", user: 3},
+        {id:9,start:"", end: "", user: 3},
+        {id:10,start:"", end: "", user: 3},
+    ]
+  },
+}
+</script>
+
+
+<template>
+  <div>
+    WorkingTimes kjhdsqkjdhqskjdh
+  </div>
+</template>
+
+<style scoped>
+
+</style>
