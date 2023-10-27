a2963dbfcf90c5bba09392c3e429727df700ac9d
inconnu060217 <jean-francois.traore@epitech.eu>
2023-10-27 17:09:17 +0200

    [ADD]defatul select start and end for workingtimes show all data

--- client/src/components/pages/WorkingTimes.vue
+++ client/src/components/pages/WorkingTimes.vue
@@ -6,29 +6,35 @@ export default {
   data() {
       return {
         workingTimes: WorkingTimeStore(state => state.workingTimes),
-        start: null,
-        end: null
+        dataSend: {
+          id: null,
+          start: null,
+          end: null
+        },
+
       }
+  },
+  methods: {
+    onChange(field, event) {
+      if (field === 'start') {
+        this.dataSend.start = event.target.value.replace("T", " ") + ":00";
+        WorkingTimeStore(state => state.getWorkingTimeByUserId(this.dataSend))
+      } else if (field === 'end') {
+        this.dataSend.end = event.target.value.replace("T", " ") + ":00";
+        WorkingTimeStore(state => state.getWorkingTimeByUserId(this.dataSend))
+      }
+    }
   },
   mounted() {
     const today = new Date();
     today.setHours(2, 0, 0, 0)
     const isoDateToday = today.toISOString().split('.')[0];
-
     let firstDayOfCurrentMonth = new Date(today.getFullYear(), today.getMonth(), 1, 2,0,0);
     let isoDateFirstDay = firstDayOfCurrentMonth.toISOString().split('.')[0];
-
-    this.start = isoDateFirstDay.replace("T", " ");
-    this.end = isoDateToday.replace("T", " ");
-
-
-
-    let dataSend = {
-      id: this.$route.params.id,
-      start: this.start,
-      end: this.end
-    }
-    WorkingTimeStore(state => state.getWorkingTimeByUserId(dataSend))
+    this.dataSend.id = this.$route.params.id
+    this.dataSend.start = isoDateFirstDay.replace("T", " ");
+    this.dataSend.end= isoDateToday.replace("T", " ");
+    WorkingTimeStore(state => state.getWorkingTimeByUserId(this.dataSend))
   },
 }
 </script>
@@ -36,6 +42,15 @@ export default {
 
 <template>
   <div>
+    <div>
+      <label for="start">start:</label>
+      <input type="datetime-local" id="start" v-model="dataSend.start" @change="onChange('start', $event)">
+    </div>
+    <div>
+      <label for="end">end:</label>
+      <input type="datetime-local" id="end" v-model="dataSend.end" @change="onChange('end', $event)">
+    </div>
+
     <div>
       <table>
         <thead>
