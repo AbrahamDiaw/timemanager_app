91f4ee527a01abbd1cd3973add353479ba154e34
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-10-30 12:34:13 +0100

    feat: Docker client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,11 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030
+    host: true,
+    port: 3030,
+    watch: {
+      usePolling: true
+    }
   },
   plugins: [vue()]
 });
