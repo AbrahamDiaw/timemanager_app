f68ec14789982eccb44b9cd152e6321d862e90af
inconnu060217 <jean-francois.traore@epitech.eu>
2023-11-02 16:25:17 +0100

    seeders

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias TIME_MANAGER.Repo
+alias TIME_MANAGER.Models.User
+
+# general_manager
+Repo.insert! %User{
+  username: "ben",
+  email: "ben.doe@gmail.com",
+  role: :general_manager,
+  password_hash: "$2b$12$hIFbngtcUwQQ4ARJgVKVWeVkEi7Uv3se6yieXrQg2O6V5s26uq37e"
+}
+
+# manager
+Repo.insert! %User{
+  username: "chien",
+  email: "chien.doe@gmail.com",
+  role: :manager,
+  password_hash: "$2b$12$hIFbngtcUwQQ4ARJgVKVWeVkEi7Uv3se6yieXrQg2O6V5s26uq37e"
+}
+
+# employee
+Repo.insert! %User{
+  username: "chat",
+  email: "chat.doe@gmail.com",
+  role: :employee,
+  password_hash: "$2b$12$hIFbngtcUwQQ4ARJgVKVWeVkEi7Uv3se6yieXrQg2O6V5s26uq37e"
+}
