f8ae16e94d910bbd3b199c6525d3649575947002
AbrahamDiaw <diawabrahampro@gmail.com>
2023-11-13 15:17:25 +0100

    fix: url

--- client/vars/Urls.ts
+++ client/vars/Urls.ts
@@ -1,4 +1,6 @@
 export enum GlobalVars {
-    BASE_URL = "http://localhost:3030/",
-    BASE_URL_API = "http://localhost:4000/api/"
+	BASE_URL_LOCAL = "http://localhost:3030/",
+	BASE_URL = "http://13.39.84.25:3030/",
+	BASE_URL_API_LOCAL = "http://localhost:4000/api/",
+	BASE_URL_API = "http://13.39.84.25:4000/api/"
 }
